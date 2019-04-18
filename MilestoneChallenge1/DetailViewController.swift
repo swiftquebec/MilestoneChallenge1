b782aeb8861c23b5f3ab413039b2f601e6bccc0d
Gregory Leck <greg@swiftquebec.com>
2019-04-18 18:40:49 -0400

    First Commit

---
+++ MilestoneChallenge1/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  MilestoneChallenge1
+//
+//  Created by Gregory Leck on 2019-04-18.
+//  Copyright © 2019 Gregory Leck. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var titleText: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+}
---
+++ MilestoneChallenge1/TableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TableViewController.swift
+//  MilestoneChallenge1
+//
+//  Created by Gregory Leck on 2019-04-18.
+//  Copyright © 2019 Gregory Leck. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    var mapPictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Canadian Flagz"
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasSuffix("png") {
+                mapPictures.append(item)
+            }
+        }
+        print(mapPictures.count)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mapPictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Flag", for: indexPath)
+        cell.textLabel?.text = mapPictures[indexPath.row]
+        
+        return cell
+    }
+    
+    // What happens when a row is touched
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = mapPictures[indexPath.row]
+            vc.titleText = "Picture"
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+}
